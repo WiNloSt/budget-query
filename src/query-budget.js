6100eb4347e4fd5a73d68107914abe6c89102aa0
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-07-14 22:52:31 +0700

    :recycle: iterate over budgets in stead period

--- src/query-budget.js
+++ src/query-budget.js
@@ -1,4 +1,5 @@
 import moment from 'moment'
+import * as R from 'ramda'
 
 export class Budgets {
   budgets = {}
@@ -6,31 +7,21 @@ export class Budgets {
   query(startDate, endDate) {
     const start = moment(startDate, 'YYYY-MM-DD')
     const end = moment(endDate, 'YYYY-MM-DD')
-
-    return this.calculateBudget(start, end)
-  }
-
-  calculateBudget(start, end) {
     const queryPeriod = new Period(start, end)
 
-    let total = 0
-    const monthDiff = end.diff(start, 'months')
-    for (let month = 0; month <= monthDiff; month++) {
-      total += new Budget(
-        this.budgets,
-        queryPeriod,
-        new Period(
-          moment(start)
-            .add(month, 'month')
-            .startOf('month'),
-          moment(start)
-            .add(month, 'month')
-            .endOf('month')
-        )
-      ).calculateBudget()
-    }
+    return this.queryPeriod(queryPeriod)
+  }
 
-    return total
+  queryPeriod(period) {
+    return R.compose(
+      R.sum,
+      R.map(budget => budget.getOverlappingBudget(period)),
+      budgets =>
+        Object.keys(budgets).map(month => {
+          const value = budgets[month]
+          return new Budget(moment(month, 'YYYY-MM'), value)
+        })
+    )(this.budgets)
   }
 }
 
@@ -52,27 +43,21 @@ class Period {
 
     if (a.end > b.end) return b.getDays()
 
-    return a.end.diff(b.start, 'days') + 1
+    return new Period(b.start, a.end).getDays()
   }
 }
 
 class Budget {
-  constructor(budgets, queryPeriod, monthPeriod) {
-    this.budgets = budgets
-    this.queryPeriod = queryPeriod
-    this.monthPeriod = monthPeriod
+  constructor(date, value) {
+    this.date = date
+    this.amount = value
   }
 
-  getIntersectedDays() {
-    if (this.queryPeriod) return this.queryPeriod.getIntersectedDays(this.monthPeriod)
-    return this.end.diff(this.start, 'days') + 1
-  }
+  getOverlappingBudget(period) {
+    const overlappingDays = period.getIntersectedDays(
+      new Period(moment(this.date).startOf('month'), moment(this.date).endOf('month'))
+    )
 
-  calculateBudget() {
-    const numberOfDaysInStartMonth = this.getIntersectedDays()
-    const amountDaysFirst = this.monthPeriod.start.daysInMonth()
-    const firstMonthBudget = this.budgets[this.monthPeriod.start.format('YYYY-MM')] || 0
-
-    return numberOfDaysInStartMonth * (firstMonthBudget / amountDaysFirst)
+    return overlappingDays * (this.amount / this.date.daysInMonth())
   }
 }
